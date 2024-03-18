94e8b14f7d379b78cb27beaad80c314e138d6d5e
sk <shuangkai.chang@recurvedata.com>
2024-03-18 14:33:09 +0800

    feat: add code for chapter 6-12

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+
+export default function loading() {
+    return <DashboardSkeleton/>
+}
--- app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h1>Da Dun Dun</h1>
-}
